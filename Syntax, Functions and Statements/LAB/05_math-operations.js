1fc29b4323bc00a490214c9de283bca57ff27de4
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-12 16:06:06 +0200

    Create 05_math-operations.js

---
+++ Syntax, Functions and Statements/LAB/05_math-operations.js
@@ -0,0 +1,14 @@
+function solve(a, b, operator) {
+    let result = 0;
+
+    switch(operator) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
+        case '%': result = a % b; break;
+        case '**': result = a**b; break;
+    }
+    console.log(result)
+}
+solve(3, 5.5, '*')
