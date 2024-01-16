2e86cbbcb7a68700cb6dd2d9d1fdd5e9a2b5582a
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-16 20:24:59 +0200

    Create 01_even-position-element.js

---
+++ Arrays and Nested Arrays/LAB/01_even-position-element.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i+=2) {
+        newArr.push(arr[i])
+    }
+    console.log(newArr.join(' '))
+}
+solve(['20', '30', '40', '50', '60'])
