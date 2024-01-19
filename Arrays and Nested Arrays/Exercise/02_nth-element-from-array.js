3efe0f686824a4112bfbb5bc91664549faff6761
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-19 22:36:45 +0200

    Create 02_nth-element-from-array.js

---
+++ Arrays and Nested Arrays/Exercise/02_nth-element-from-array.js
@@ -0,0 +1,10 @@
+function printNthElement(arr, n) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += n) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+printNthElement(
+  ["5", "20", "31", "4", "20"], 2
+);
