d1fe2c2938b8747d2c467896504c4048ed8bfe8b
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-21 22:02:03 +0200

    Create 06_list-of-names.js

---
+++ Arrays and Nested Arrays/Exercise/06_list-of-names.js
@@ -0,0 +1,13 @@
+function listOfNames(arr) {
+    let sortedNames = arr.sort((a,b) => a.localeCompare(b));
+    for(let i = 0; i < sortedNames.length; i++) {
+        console.log(`${i+1}.${sortedNames[i]}`)
+    }
+}
+listOfNames(["John",
+
+"Bob",
+
+"Christina",
+
+"Ema"])
