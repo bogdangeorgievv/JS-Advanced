88dcd1c3e341fdb6ed4d69b7dd227888eb4162cb
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-21 22:03:35 +0200

    Create 08_sort-an-array-by-2-criteria.js

---
+++ Arrays and Nested Arrays/Exercise/08_sort-an-array-by-2-criteria.js
@@ -0,0 +1,6 @@
+function solve(arr) {
+
+    let sorted = arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+    console.log(sorted.join('\n'));
+}
+solve(['test', 'Deny', 'omen', 'Default'])
