786621e642e17b725ae7c18d321f9787a88caf67
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-22 21:30:03 +0200

    Create 01_calorie-object.js

---
+++ Objects and Composition/Exercise/01_calorie-object.js
@@ -0,0 +1,11 @@
+function calorieObject(input) {
+  let result = {};
+  for (let i = 0; i < input.length; i += 2) {
+    let name = input[i];
+    let calories = Number(input[i + 1]);
+    result[name] = calories;
+  }
+  console.log(result);
+}
+calorieObject(["Yoghurt", "48", "Rise", "138", "Apple", "52"]);
+// calorieObject(["Potato", "93", "Skyr", "63", "Cucumber", "18", "Milk", "42"]);
