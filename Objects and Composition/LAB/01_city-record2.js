db461e7f35c4bce01476e073f89682252719a640
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-22 21:26:54 +0200

    Create 01_city-record2.js

---
+++ Objects and Composition/LAB/01_city-record2.js
@@ -0,0 +1,13 @@
+function cityRecord(name, population, treasury) {
+    let result = {
+        name,
+        population,
+        treasury
+    }
+    return result;
+}
+console.log(cityRecord('Tortuga',
+
+7000,
+
+15000));
