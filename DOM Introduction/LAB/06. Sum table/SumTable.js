b60b505a5348ebf1352108e0d50fd1d1d66d195c
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-01-29 10:37:12 +0200

    Create SumTable.js

---
+++ DOM Introduction/LAB/06. Sum table/SumTable.js
@@ -0,0 +1,11 @@
+function sumTable() {
+    let table = document.querySelectorAll('table tr');
+    let sum = 0;
+
+    for(let i = 1; i < table.length - 1; i++) {
+        let cols = table[i].children;
+        let cost = cols[cols.length - 1].textContent;
+        sum += Number(cost)
+    }
+    document.getElementById('sum').textContent = sum;
+}
