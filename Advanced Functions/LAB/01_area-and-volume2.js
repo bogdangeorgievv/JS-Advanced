01e360c5ff8cb9eb1249ed8ce59496a3f9f2e090
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2024-02-15 12:19:47 +0200

    Create 01_area-and-volume2.js

---
+++ Advanced Functions/LAB/01_area-and-volume2.js
@@ -0,0 +1,27 @@
+function solve(areaFn, volFn, input) {
+    const data = JSON.parse(input);
+    let result = data.map(figure => ({   
+        area: areaFn.call(figure),
+        volume: volFn.call(figure)
+    }))
+    console.log(result)
+}
+solve(area, vol, `[ 
+    {"x":"1","y":"2","z":"10"}, 
+    {"x":"7","y":"7","z":"10"}, 
+    {"x":"5","y":"2","z":"10"} 
+    ]`)
+solve(area, vol, `[
+    {"x":"10","y":"-22","z":"10"},
+    {"x":"47","y":"7","z":"-5"},
+    {"x":"55","y":"8","z":"0"},
+    {"x":"100","y":"100","z":"100"},
+    {"x":"55","y":"80","z":"250"}
+    ]`)
+
+function area() {
+    return Math.abs(this.x * this.y);
+};
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
